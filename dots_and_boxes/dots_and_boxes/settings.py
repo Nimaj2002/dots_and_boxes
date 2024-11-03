2fa6499c5c79cde3211e00c7ba4e5260cf757962
Nima Jelodari <nimajelodari2002@gmail.com>
2024-11-03 09:31:24 +0330

    updated server (other hosts can run this)

--- dots_and_boxes/dots_and_boxes/settings.py
+++ dots_and_boxes/dots_and_boxes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1(0lt#n*ux(lhw3lvu&sn83#hi4g6p0_vtc+k=qum3l!o8c-e7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
